6581169bfcb54f9777ee13bb1494b1d7b4104d97
mraggi <mraggi@gmail.com>
2016-03-08 09:08:28 -0600

    main

--- main.cpp
+++ main.cpp
@@ -9,35 +9,11 @@ using namespace std;
 int main(int argc, char **argv) 
 {	
 	using namespace dscr;
-// 	using combination = combinations::combination; 
-// 
-// 	// 8,233,430,727,600 combinaciones
-// 	combinations X(46,23); 
-// 
-// 	auto it = std::partition_point(X.begin(), X.end(), 
-// 		[](const combination& x){
-// 		if (x.back() < 36)
-// 			return true;
-// 		return false;
-// 	});
-// 	cout << *it << endl;
-// 	return 0;
-	
-	Chronometer();
-	testManualCombinations();
-	
-// 	lluint i = 0;
-// 	combinations X(33,16);
-// 	for (auto& x : X)
-// 	{
-// 		if (x.back() == 25)
-// 			++i;
-// 	}
-// 	cout << "ttnum= " << i << endl;
-// 	cout << "\n================ Starting Correctness Tests ================" << endl;
-// 	testCorrectness();
-// 	cout << "\n================ Starting Speed Tests ================" << endl;
-// 	testSpeed();
+
+	cout << "\n================ Starting Correctness Tests ================" << endl;
+	testCorrectness();
+	cout << "\n================ Starting Speed Tests ================" << endl;
+	testSpeed();
 	
 	cout << "Total Time taken = " << dscr::TimeFromStart() << "s" << endl;
 	return 0;
