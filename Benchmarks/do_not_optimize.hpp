e4ffac39d63f9966fe7e0086aa35834ab0fc0b21
mraggi <mraggi@gmail.com>
2017-12-06 16:40:00 -0600

    Added a little notification to do_not_optimize

--- Benchmarks/do_not_optimize.hpp
+++ Benchmarks/do_not_optimize.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+//Copied directly from google benchmark, who in turn copied it from Chandler Carruth's talk. I'm not 100% sure if I'm allowed to do this, but whatever, it's 3 lines of code.
+
 template <class Tp>
 inline void DoNotOptimize(Tp const& value) {
   // Clang doesn't like the 'X' constraint on `value` and certain GCC versions
