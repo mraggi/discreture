107a89a40caf4e5645575cf78cb9eaebaa5b6e06
mraggi <mraggi@gmail.com>
2017-12-10 15:36:45 -0600

    WTF? Why missing template parameters? GRR

--- tests/partition_tests.cpp
+++ tests/partition_tests.cpp
@@ -13,7 +13,7 @@ void check_partition(const partitions::partition& x, int n)
 		
 	ASSERT_EQ(suma,n);
 	
-	ASSERT_TRUE(std::is_sorted(x.begin(), x.end(), std::greater<>()));
+	ASSERT_TRUE(std::is_sorted(x.begin(), x.end(), std::greater<int>()));
 
 }
 
