335e33cfc97ece4fa81b89824bfbe727286bfc20
mraggi <mraggi@gmail.com>
2018-05-18 09:30:34 -0500

    Updated cmakelists

--- clang-tidy-helper.sh
+++ clang-tidy-helper.sh
@@ -4,5 +4,5 @@ rm -rf build-clang-tidy
 mkdir build-clang-tidy
 cd build-clang-tidy
 
-cmake -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=ON -DBUILD_BENCHMARKS=ON -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_FLAGS -DCMAKE_CXX_CLANG_TIDY:STRING="clang-tidy;--header-filter=.*;-checks=*,-google-readability-braces-around-statements,-readability-braces-around-statements,-fuchsia-*,-hicpp-braces-around-statements,-llvm-header-guard,-misc-unused-parameters,-google-runtime-references" ..
+cmake -DBUILD_TESTS=OFF -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_CLANG_TIDY:STRING="clang-tidy;--header-filter=.*;-checks=*,-google-readability-braces-around-statements,-readability-braces-around-statements,-fuchsia-*,-hicpp-braces-around-statements,-llvm-header-guard,-misc-unused-parameters,-google-runtime-references,-google-explicit-constructor,-hicpp-explicit-conversions" ..
 make
