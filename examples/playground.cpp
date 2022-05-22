5638af42c460a28adc8515d6e44a4d53547f77cd
Miguel Raggi <mraggi@gmail.com>
2022-05-22 17:53:03 -0500

    dont know, github-desktop says tehre are changes

--- examples/playground.cpp
+++ examples/playground.cpp
@@ -1,20 +1,44 @@
-// This file is my personal playground. I just use it to test stuff out.
+#include <iostream>
+#include <execution>
+#include <vector>
+#include <boost/iterator/iterator_facade.hpp>
 
-#include "Discreture/Combinations.hpp"
-#include <string_view>
+class n_iterator
+    : public boost::iterator_facade<n_iterator, const int&, boost::random_access_traversal_tag>
+{
+public:
+    explicit n_iterator(int t = 0) : value_(t) {}
 
-using namespace std; // NOLINT
-using namespace discreture; // NOLINT
-using namespace discreture::detail; // NOLINT
+private:
+    void increment() { ++value_; }
+
+    void decrement() { --value_; }
+
+    const int& dereference() const { return value_; }
+
+    void advance(int n) { value_ += n; }
+
+    bool equal(const n_iterator& other) const
+    {
+        return value_ == other.value_;
+    }
+
+    int distance_to(const n_iterator& other) const
+    {
+        return static_cast<int>(other.value_) - value_;
+    }
+
+private:
+    int value_{0};
+
+    friend class boost::iterator_core_access;
+}; // end class iterator
 
 int main()
 {
-    std::string X = "Hello world!";
-    std::string_view A(X);
-    std::cout << A << std::endl;
-
-    for (auto&& a : discreture::combinations(A, 3))
-        cout << a << endl;
-
-    return 0;
+	std::for_each(std::execution::par, n_iterator(0), n_iterator(10), [&](auto i) {
+		std::cout << i << '\n';
+		});
+	std::cout << "done" << std::endl;
+	return 0;
 }
